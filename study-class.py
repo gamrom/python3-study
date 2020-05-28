1d168929202b559e2c84dcd623cc301be88e6a0e
gamrom <gamrom01@naver.com>
2020-05-28 09:36:33 +0900

    class pratice

---
+++ study-class.py
@@ -0,0 +1,28 @@
+class Unit:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+
+class AttackUnit(Unit):
+    def __init__(self, name, hp, damage):
+        Unit.__init__(self, name, hp)
+        self.damage = damage
+
+    def attack(self, power):
+        print(f"{power}만큼 공격을 합니다")
+
+class DefenceUnit(Unit):
+    def __init__(self, name, hp, guard):
+        Unit.__init__(self, name, hp)
+        self.guard = guard
+
+    def defence(self, power):
+        print(f"{power}만큼 방어를 합니다")
+
+# print(DefenceUnit("디펜스유닛1", 100, 10).defence(30))
+class SpecialUnit(AttackUnit, DefenceUnit):
+    def __init__(self, name, hp, damage, guard):
+        AttackUnit.__init__(self, name, hp, damage)
+        DefenceUnit.__init__(self, name, hp, guard)
+
+unit1 = SpecialUnit("스페셜유닛", 100, 30, 20)
